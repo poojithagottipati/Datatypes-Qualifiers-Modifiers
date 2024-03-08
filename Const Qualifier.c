260b4c16fa63475aff60f086c0e202c0f5227f41
poojithagottipati <160849305+poojithagottipati@users.noreply.github.com>
2024-03-08 11:33:34 +0530

    Create Const Qualifier.c

---
+++ Const Qualifier.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    const int MAX_VALUE = 100; 
+    const float PI = 3.14159;
+
+    printf("Max Value: %d\n", MAX_VALUE);
+    printf("Value of PI: %f\n", PI);
+
+    return 0;
+}
+
+/*output
+Max Value: 100
+Value of PI: 3.141590
+*/
